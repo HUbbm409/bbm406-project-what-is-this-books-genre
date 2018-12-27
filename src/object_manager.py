1fe372bfe854873bb3bdff92ce8ef920d9ba76c7
b21427559 <b21427559@cs.hacettepe.edu.tr>
2018-12-27 22:59:58 +0300

    Create object_manager.py

---
+++ src/object_manager.py
@@ -0,0 +1,18 @@
+import pickle
+
+class ObjectManager:
+    def __init__(self):
+        pass
+    # record the object
+    def RecordObject(self,myObject, modelName):
+        fileName = modelName
+        fileObject = open(fileName, 'wb')
+        pickle.dump(myObject, fileObject)
+        fileObject.close()
+
+
+    # read the object
+    def ReadObject(self,modelName):
+        fileObject = open(modelName, 'rb')
+        b = pickle.load(fileObject)
+        return b
